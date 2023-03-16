bd86607f43239b7b3d11ecf47f1645ab2bad17f8
Bob Ware <91074643+bobwareinc@users.noreply.github.com>
2023-03-16 13:42:59 -0400

    Add files via upload

---
+++ YT Audio Snipper.py
@@ -0,0 +1,19 @@
+from pytube import YouTube
+import os
+
+def download_audio(video_url: str, start_time: str, end_time: str):
+    yt = YouTube(video_url)
+    stream = yt.streams.filter(only_audio=True).first()
+    filename = f'{yt.title}.mp4'
+    stream.download(filename=filename)
+    
+    new_filename = filename.rsplit('.', 1)[0] + '.wav'
+    
+    start_time_seconds = sum(int(x) * 60 ** i for i,x in enumerate(reversed(start_time.split(':'))))
+    end_time_seconds = sum(int(x) * 60 ** i for i,x in enumerate(reversed(end_time.split(':'))))
+    
+    os.system(f'ffmpeg -y -i "{filename}" -ss {start_time_seconds} -to {end_time_seconds} "{new_filename}"')
+    os.remove(filename)
+
+# Example usage:
+download_audio('https://www.youtube.com/watch?v=H-QdAG8ts2g', '00:25:18', '00:25:42')
